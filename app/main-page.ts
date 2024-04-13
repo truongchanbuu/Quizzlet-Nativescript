976592c183f14cca20923f68b464842fede862ce
truongchanbuu <truongchanbuu0512@gmail.com>
2024-04-13 17:46:28 +0700

    Add main page

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,7 +1,12 @@
 import { EventData, Page } from '@nativescript/core'
 import { HelloWorldModel } from './main-view-model'
+import { TabSelectedEventData } from '@nativescript-community/ui-material-bottomnavigationbar'
 
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object
   page.bindingContext = new HelloWorldModel()
+}
+
+export function tabSelected(args: TabSelectedEventData) {
+  
 }
